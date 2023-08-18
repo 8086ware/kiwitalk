50e33e76d7a8e94265e86173e92da3f317914731
8086ware <kirillos.khan08@outlook.com>
2023-08-18 14:21:27 -0400

    Initial Commit

---
+++ src/client/main.c
@@ -0,0 +1,93 @@
+#include <sockmanip.h>
+#include <termmanip.h>
+#include <string.h>
+
+#define KIWITALK_PORT "47831"
+
+int main(void) {
+	sm_win_init();
+	tm_init();
+	
+	int scr_cols, scr_rows;
+
+	tm_get_scrsize(&scr_cols, &scr_rows);
+	_Bool menu = 1;
+	_Bool connected = 0;
+
+	Sm_server* server = NULL;
+
+	Tm_window* main_window = tm_window(1, 1, scr_cols - 2, scr_rows - 2);
+	tm_win_flags(main_window, TM_FLAG_SCROLL, 1);
+
+	tm_win_attrib(main_window, TM_ATTRIB_FG_BLUE, 1);
+
+	tm_win_border(main_window);
+
+	tm_win_attrib(main_window, TM_ATTRIB_FG_BLUE, 0);
+
+	tm_win_update(main_window);
+
+	Tm_window* main_window_text = tm_window(1, 1, tm_win_get_columns(main_window) - 2, tm_win_get_rows(main_window) - 2);
+	Tm_window* main_window_input = tm_window(0, tm_win_get_rows(main_window_text) - 1, tm_win_get_columns(main_window_text), 1);
+
+	tm_win_parent(main_window, main_window_text, TM_CHILD_NORMAL);
+	tm_win_parent(main_window, main_window_input, TM_CHILD_NORMAL);
+
+	tm_win_print(main_window_text, "-- KIWITALK --\n\nWelcome. Type /help for list of commands.");
+	
+	tm_win_update(main_window_text);
+
+	while(menu) {
+		char command[1024];
+
+		tm_win_clear(main_window_input);
+		tm_win_print(main_window_input, "> ");
+		tm_win_input_str(main_window_input, command, 1024);
+		tm_win_print(main_window_text, "\n");
+
+		char* arg1;
+		char* arg2;
+		char* arg3;
+		char* arg4;
+		char* arg5;
+
+		arg1 = strtok(command, " ");
+		arg2 = strtok(NULL, " ");
+		arg3 = strtok(NULL, " ");
+		arg4 = strtok(NULL, " ");
+		arg5 = strtok(NULL, " ");
+
+		if(strcmp(arg1, "/help") == 0) {
+			tm_win_print(main_window_text, "COMMANDS:\n\nconnect [HOSTNAME] - Will ask for a server hostname to connect to\nexit - Exit kiwitalk");
+		}
+
+		else if(strcmp(arg1, "/exit") == 0) {
+			tm_win_free(main_window);
+			tm_exit();
+			return 0;
+		}
+
+		else if(strcmp(arg1, "/connect") == 0) {
+			tm_win_print(main_window_text, "Attempting to connect...\n");
+			tm_win_update(main_window);
+
+			server = sm_server(arg2, KIWITALK_PORT, SM_SERVER_CONNECT, SM_TCP);
+
+			if(server == NULL) {
+				tm_win_print(main_window_text, "Couldn't connect to server %s.\n", arg2);
+			}
+
+			else {
+				connected = 1;
+			}
+		}
+
+		if(connected) {
+			
+		}
+
+		tm_win_update(main_window);
+	}
+
+	tm_exit();
+}
---
+++ src/server/main.c
@@ -0,0 +1,45 @@
+#include <sockmanip.h>
+#include <termmanip.h>
+#include <memory.h>
+
+#define KIWITALK_PORT "47831"
+
+int main(void) {
+	tm_init();
+	Sm_server* server = sm_server(NULL, KIWITALK_PORT, SM_SERVER_CREATE, SM_TCP);
+
+	if(server == NULL) {
+		tm_print("Couldn't host server on port %s. Is another program using the port?", KIWITALK_PORT);
+		tm_input_ch();
+		return 1;
+	}
+
+	tm_print("Listening on port %s\n", KIWITALK_PORT);
+	tm_update();
+
+	while(1) {
+		if(sm_new_client(server, 10)) {
+			struct sockaddr_storage addr;
+			memset(&addr, 0, sizeof(addr));
+			socklen_t addr_len = sizeof(addr);
+
+			tm_attrib(TM_ATTRIB_FG_GREEN, 1);
+
+			if(getsockname(sm_get_client_socket(*sm_get_server_client(server, sm_get_server_client_amount(server) - 1)), (struct sockaddr*)&addr, &addr_len) != -1) {
+				char ip[128];
+				char port[16];
+
+				if(getnameinfo((struct sockaddr*)&addr, addr_len, ip, 128, port, 16, NI_NUMERICHOST) == 0) {
+					tm_print("Connection from %s:%s\n", ip, port);
+				}
+			}
+	
+			else {
+				tm_print("Connection from Unknown Host (Couldn't get socket name!\n");
+			}
+
+			tm_attrib(TM_ATTRIB_FG_GREEN, 0);
+			tm_update();
+		}
+	}
+}
