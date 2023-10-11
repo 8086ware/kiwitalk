9de1f83b72165e40b43d13d28b415e26f235cd11
8086ware <kirillos.khan08@outlook.com>
2023-10-10 23:20:38 -0400

    Adjust code to read for control characters for enter and backspace and check if the terminal is resized properly

--- src/client/tab/getinput.c
+++ src/client/tab/getinput.c
@@ -5,15 +5,15 @@
 void get_tab_input(struct Tab*** tabs, int tab_number, int tab_amount, char* command, int max_size) {
 	struct Tab* tab = (*tabs)[tab_number];
 
-	int c = tm_win_input_ch(tab->window_input);
+	Tm_input c = tm_win_input(tab->window_input);
 
-	if(c == '\n' || c == '\r') {
+	if((c.ctrl_down && (c.key == 'J')) || c.key == TM_KEY_LF) {
 		command[tab->input_amount] = '\0';
 		tab->finished_input = 1;
 		tab->input_amount = 0;
 	}
 
-	else if(c == '\b' || c == '\177') {
+	else if((c.ctrl_down && c.key == 'H') || c.key == TM_KEY_DEL) {
 		if(tab->input_amount > 0) {
 			tab->input_amount--;
 
@@ -27,24 +27,21 @@ void get_tab_input(struct Tab*** tabs, int tab_number, int tab_amount, char* com
 		}
 	}
 
-	else if(c > 0 && c < 128 && tab->input_amount < max_size) {
-		tm_win_print(tab->window_input,"%c", c);
-		tm_win_update(tab->window_input);
-		command[tab->input_amount] = c;
+	else if(c.key > 0 && c.key < 128 && tab->input_amount < max_size) {
+		tm_win_print(tab->window_input, "%c", c.key);
+		command[tab->input_amount] = c.key;
 		tab->input_amount++;
 	}
 
-	else if(c == TM_KEY_SCREEN_RESIZED) {
+	else if(c.terminal_resized) {
 		int scr_cols, scr_rows;
 		tm_get_scrsize(&scr_cols, &scr_rows);
 	
 		for(int i = 0; i < tab_amount; i++) { 
-			tm_win_modify((*tabs)[i]->window_text, 0, 0, scr_cols, scr_rows - 1);
+			tm_win_modify((*tabs)[i]->window_text, 0, 0, scr_cols, scr_rows);
 			tm_win_modify((*tabs)[i]->window_input, 0, scr_rows - 1, scr_cols, 1);
 			tm_win_print((*tabs)[i]->window_input, "> ");
 		}
-
-		tm_win_update(tab->window_text);
-		tm_win_update(tab->window_input);
 	}
 }
+
