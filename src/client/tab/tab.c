9a3aa569d7d837c2e76e8499c3730dfbd1919e09
8086ware <kirillos.khan08@outlook.com>
2023-11-10 19:03:53 -0500

    Make code cleaner by using tab instead of (*tabs)[*amount]

--- src/client/tab/tab.c
+++ src/client/tab/tab.c
@@ -23,25 +23,28 @@ void new_tab(struct Tab*** tabs, int* amount) {
 
 	tm_get_scrsize(&scr_cols, &scr_rows);
 
-	((*tabs)[*amount])->server = NULL;
+	struct Tab* tab = (*tabs)[*amount];
 
-	((*tabs)[*amount])->window_text = tm_window(0, 0, scr_cols, scr_rows);
-	((*tabs)[*amount])->window_input = tm_window(0, tm_win_get_rows(((*tabs)[*amount])->window_text) - 1, tm_win_get_columns(((*tabs)[*amount])->window_text), 1);
+	tab->server = NULL;
 
-	tm_win_flags(((*tabs)[*amount])->window_text, TM_FLAG_SCROLL, 1);
-	tm_win_flags(((*tabs)[*amount])->window_text, TM_FLAG_ECHO, 0);
+	tab->window_text = tm_window(0, 0, scr_cols, scr_rows);
+	tab->window_input = tm_window(0, tm_win_get_rows(tab->window_text) - 1, tm_win_get_columns(tab->window_text), 1);
 
-	tm_win_input_timeout(((*tabs)[*amount])->window_input, 1); 
+	tm_win_flags(tab->window_text, TM_FLAG_SCROLL, 1);
+	tm_win_flags(tab->window_text, TM_FLAG_ECHO, 0);
 
-	tm_win_parent(((*tabs)[*amount])->window_text, ((*tabs)[*amount])->window_input, TM_CHILD_NORMAL);
+	tm_win_input_timeout(tab->window_input, 1); 
 
-	tm_win_print(((*tabs)[*amount])->window_text, "-- KIWITALK --\n\nWelcome. Type /help for list of commands.\n");
+	tm_win_parent(tab->window_text, tab->window_input, TM_CHILD_NORMAL);
 
-	((*tabs)[*amount])->connected = 0;
-	((*tabs)[*amount])->finished_input = 0;
-	((*tabs)[*amount])->input_amount = 0;
+	tm_win_print(tab->window_text, "-- KIWITALK --\n\nWelcome. Type /help for list of commands.\n");
 
-	tm_win_print(((*tabs)[*amount])->window_input, "> ");
+	tab->connected = 0;
+	tab->finished_input = 0;
+	tab->input_amount = 0;
 
+	tm_win_print(tab->window_input, "> ");
+
+	tm_win_update(tab->window_text);
 	(*amount)++;
 }
