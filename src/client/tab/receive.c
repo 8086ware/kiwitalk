e00e3951dbf69d7f472cbac1915ac67e3c82334d
8086ware <kirillos.khan08@outlook.com>
2023-11-19 17:30:01 -0500

    Print out the time when someone joins, leaves, or messages

--- src/client/tab/receive.c
+++ src/client/tab/receive.c
@@ -3,6 +3,7 @@
 #include <sockmanip.h>
 #include <string.h>
 #include "parse_command.h"
+#include <time.h>
 
 void receive_tab_server_requests(struct Tab*** tabs, int* tab_selected, int* amount) {
 	struct Tab* tab = (*tabs)[*tab_selected];
@@ -17,14 +18,20 @@ void receive_tab_server_requests(struct Tab*** tabs, int* tab_selected, int* amo
 			int request_arg_amount = 0;
 			char** request_args = parse_command(receive_buffer, &request_arg_amount, "\177");
 
+			time_t e_time;
+			time(&e_time);
+			struct tm* time = localtime(&e_time);
+
 			if(strcmp(request_args[0], "MSG") == 0) {
 				tm_win_attrib(tab->window_text, TM_ATTRIB_FG_BRIGHTWHITE, 1);
+				tm_win_print(tab->window_text, "[%d:%d:%d] ", time->tm_hour, time->tm_min, time->tm_sec);
 				tm_win_print(tab->window_text, "%s ~> %s\n", request_args[1], request_args[2]);
 				tm_win_attrib(tab->window_text, TM_ATTRIB_FG_BRIGHTWHITE, 0);
 			}
 
 			if(strcmp(request_args[0], "JOIN") == 0) {
 				tm_win_attrib(tab->window_text, TM_ATTRIB_FG_GREEN, 1);
+				tm_win_print(tab->window_text, "[%d:%d:%d] ", time->tm_hour, time->tm_min, time->tm_sec);
 				tm_win_print(tab->window_text, "-> %s joined from %s\n", request_args[1], request_args[2]);
 				tm_win_attrib(tab->window_text, TM_ATTRIB_FG_GREEN, 0);
 			}
@@ -39,6 +46,7 @@ void receive_tab_server_requests(struct Tab*** tabs, int* tab_selected, int* amo
 
 			if(strcmp(request_args[0], "LEFT") == 0) {
 				tm_win_attrib(tab->window_text, TM_ATTRIB_FG_RED, 1);
+				tm_win_print(tab->window_text, "[%d:%d:%d] ", time->tm_hour, time->tm_min, time->tm_sec);
 				tm_win_print(tab->window_text, "<- %s left\n", request_args[1]);
 				tm_win_attrib(tab->window_text, TM_ATTRIB_FG_RED, 0);
 			}
