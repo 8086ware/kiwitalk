e7f880f986b046e99067a144bc177c13ce082f41
8086ware <kirillos.khan08@outlook.com>
2023-11-19 22:49:36 -0500

    Dont detect for lost clients in send_to_clients

--- src/server/send.c
+++ src/server/send.c
@@ -7,17 +7,5 @@ void send_to_client(Sm_server* s, int client_id, char*** names, char* string, in
 	tm_update();
 
 	if(sm_send(sm_get_client_socket(*sm_get_server_client(s, client_id)), string, bytes, 0) == -1) {
-		char send_left_buf[4096];
-		int bytes_send_left = sprintf(send_left_buf, "LEFT\177%s", (*names)[client_id]);
-
-		char* temp = (*names)[sm_get_server_client_amount(s) - 1];
-		(*names)[client_id] = temp;
-
-		*names = realloc(*names, (sm_get_server_client_amount(s) - 1) * sizeof(char*));
-		sm_client_free(s, client_id);
-
-		for(int i = 0; i < sm_get_server_client_amount(s); i++) {
-			send_to_client(s, i, names, send_left_buf, bytes_send_left);
-		}
 	}
 }
