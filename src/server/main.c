314a3efc571ba30b45ccccf2938b784b990d96bd
8086ware <kirillos.khan08@outlook.com>
2023-11-25 18:55:53 -0500

    Dont use sockmanip for server and use poll and manually make the socket

--- src/server/main.c
+++ src/server/main.c
@@ -1,32 +1,60 @@
-#include <sockmanip.h>
 #include <termmanip.h>
 #include <string.h>
 #include <stdio.h>
 #include "parse_command.h"
-#include "send.h"
 #include <time.h>
 
 #define KIWITALK_PORT "47831"
 
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
 void sigpipe_hndlr() {}
+#endif
 
 int main(void) {
-#ifndef _WIN32
+#ifdef _WIN32
+	WSADATA d;
+
+	WSAStartup(MAKEWORD(2, 2), &d);
+#else
 	signal(SIGPIPE, sigpipe_hndlr);
 #endif
 
 	tm_init();
 	tm_flags(TM_FLAG_SCROLL, 1);
 
-	sm_win_init();
-	Sm_server* server = sm_server(NULL, KIWITALK_PORT, SM_SERVER_CREATE, SM_TCP, SM_IPV4);
+	struct addrinfo hints, *servinfo = NULL;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
-	if(server == NULL) {
-		tm_print("Couldn't host server on port %s. Is another program using the port?", KIWITALK_PORT);
-		tm_input();
+	if(getaddrinfo(NULL, KIWITALK_PORT, &hints, &servinfo) == -1) {
+		fprintf(stderr, "GAI Error");
 		return 1;
 	}
 
+	int server_socket = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+
+	if(bind(server_socket, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+		fprintf(stderr, "bind() error");
+		return 1;
+	}
+
+	if(listen(server_socket, 100) == -1) {
+		fprintf(stderr, "listen() error");
+		return 1;
+	}
+
+#ifdef _WIN32
+	unsigned long mode = 1;
+	ioctlsocket(server_socket, FIONBIO, &mode);
+#else
+	int flags = fcntl(tab->server_socket, F_GETFL);
+	fcntl(server_socket, F_SETFL, flags | O_NONBLOCK);
+#endif
 	tm_print("Listening on port %s\n", KIWITALK_PORT);
 	tm_update();
 
@@ -35,33 +63,64 @@ int main(void) {
 	time_t elapsed = time(NULL);
 	time_t start = time(NULL);
 
+	int poll_amount = 1;
+
+	struct pollfd* s_poll = malloc(sizeof(struct pollfd));
+
+	if(s_poll == NULL) {
+		fprintf(stderr, "Memory error");
+		return 1;
+	}
+
+	s_poll[0].fd = server_socket;
+	s_poll[0].events = POLLIN;
+
 	while(1) {
-		if(sm_new_client(server, 1)) {
-			names = realloc(names, sizeof(char*) * sm_get_server_client_amount(server));
+		WSAPoll(s_poll, poll_amount, -1);
+
+		if(s_poll[0].revents & POLLIN) {
+			names = realloc(names, sizeof(char*) * (poll_amount + 1));
 
 			if(names == NULL) {
 				fprintf(stderr, "Memory error");
 				return 2;
 			}
 
-			names[sm_get_server_client_amount(server) - 1] = malloc(256);
+			poll_amount++;
 
-			if(names[sm_get_server_client_amount(server) - 1] == NULL) {
+			names[poll_amount - 1] = malloc(256);
+
+			if(names[poll_amount - 1] == NULL) {
 				fprintf(stderr, "Memory error");
 				return 3;
 			}
 
-			sm_receive(sm_get_client_socket(*sm_get_server_client(server, sm_get_server_client_amount(server) - 1)), names[sm_get_server_client_amount(server) - 1], 256, -1);
+			struct sockaddr client_addr;
+			int client_len = sizeof(client_addr);
+
+			int client_socket = accept(server_socket, &client_addr, &client_len);
+
+			s_poll = realloc(s_poll, sizeof(struct pollfd) * (poll_amount + 1));
+
+			if(s_poll == NULL) {
+				fprintf(stderr, "Memory error");
+				return 4;
+			}
+
+			s_poll[poll_amount - 1].fd = client_socket;
+			s_poll[poll_amount - 1].events = POLLIN;
+
+			recv(client_socket, names[poll_amount - 1], 256, 0);
 
 			char ip[256];
 
-			getnameinfo(&sm_get_server_client(server, sm_get_server_client_amount(server) - 1)->addr, sm_get_server_client(server, sm_get_server_client_amount(server) - 1)->addr_len, ip, 128, NULL, 0, 0);
+			getnameinfo(&client_addr, client_len, ip, 128, NULL, 0, 0);
 
 			char send_buf[4096];
-			int bytes_to_send = sprintf(send_buf, "JOIN\177%s\177%s\177", names[sm_get_server_client_amount(server) - 1], ip);
+			int bytes_to_send = sprintf(send_buf, "JOIN\177%s\177%s\177", names[poll_amount - 1], ip);
 
-			for(int i = 0; i < sm_get_server_client_amount(server); i++) {
-				send_to_client(server, i, &names, send_buf, bytes_to_send);
+			for(int i = 1; i < poll_amount; i++) {
+				send(s_poll[i].fd, send_buf, bytes_to_send, 0);
 			}
 
 			tm_update();
@@ -70,73 +129,92 @@ int main(void) {
 		char send_buf[4096];
 		int bytes_to_send = 0;
 
-		for(int i = 0; i < sm_get_server_client_amount(server); i++) {
+		for(int i = 1; i < poll_amount; i++) {
 			char receive_buf[4096];
-			int bytes_received = sm_receive(sm_get_client_socket(*sm_get_server_client(server, i)), receive_buf, 4096, 0);
+			if(s_poll[i].revents & POLLIN) {
+				int bytes_received = recv(s_poll[i].fd, receive_buf, 4096, 0);
 
-			receive_buf[bytes_received] = '\0';
+				receive_buf[bytes_received] = '\0';
 
-			if(bytes_received > 0) {
-				int request_arg_amount = 0;
-				char** request_args = parse_command(receive_buf, &request_arg_amount, "\177");
+				if(bytes_received > 0) {
+					int request_arg_amount = 0;
+					char** request_args = parse_command(receive_buf, &request_arg_amount, "\177");
 
-				if(strcmp(request_args[0], "MSG") == 0) {
-					bytes_to_send = sprintf(send_buf, "MSG\177%s\177%s\177", names[i], request_args[1]);
+					if(strcmp(request_args[0], "MSG") == 0) {
+						bytes_to_send = sprintf(send_buf, "MSG\177%s\177%s\177", names[i], request_args[1]);
 
 
-					for(int i = 0; i < sm_get_server_client_amount(server); i++) {
-						send_to_client(server, i, &names, send_buf, bytes_to_send);
-					}
-				}
-
-				else if(strcmp(request_args[0], "LIST") == 0) {
-					bytes_to_send += sprintf(send_buf, "LIST\177");
-
-					for(int i = 0; i < sm_get_server_client_amount(server); i++) {
-						bytes_to_send += sprintf(send_buf + bytes_to_send, "%s\177", names[i]);
+						for(int i = 1; i < poll_amount; i++) {
+							send(s_poll[i].fd, send_buf, bytes_to_send, 0);
+						}
 					}
 
-					send_to_client(server, i, &names, send_buf, bytes_to_send);
-				}
+					else if(strcmp(request_args[0], "LIST") == 0) {
+						bytes_to_send += sprintf(send_buf, "LIST\177");
 
-				else if(strcmp(request_args[0], "EXIT") == 0) {
+						for(int i = 1; i < poll_amount; i++) {
+							bytes_to_send += sprintf(send_buf + bytes_to_send, "%s\177", names[i]);
+						}
+
+						send(s_poll[i].fd, send_buf, bytes_to_send, 0);
+					}
+
+					else if(strcmp(request_args[0], "EXIT") == 0) {
 						bytes_to_send = sprintf(send_buf, "LEFT\177%s", names[i]);
 
-						char* temp = names[sm_get_server_client_amount(server) - 1];
+						char* temp = names[poll_amount - 1];
 						names[i] = temp;
+						names = realloc(names, (poll_amount - 1) * sizeof(char*));
 
-						names = realloc(names, (sm_get_server_client_amount(server) - 1) * sizeof(char*));
-						sm_client_free(server, i);
+						struct pollfd temp2 = s_poll[poll_amount - 1];
+						s_poll[i] = temp2;
+						s_poll = realloc(s_poll, (poll_amount - 1) * sizeof(struct pollfd));
 
-						for(int i = 0; i < sm_get_server_client_amount(server); i++) {
-							send_to_client(server, i, &names, send_buf, bytes_to_send);
-						}
-				}
-
-				free(request_args);
-			}
-
-			else {
-				if(elapsed - start == 15) {
-					start = time(NULL);
-					elapsed = time(NULL);
-
-					if(sm_send(sm_get_client_socket(*sm_get_server_client(server, i)), ".", 1, 0) == -1) {
-						char send_left_buf[4096];
-						int bytes_send_left = sprintf(send_left_buf, "LEFT\177%s", names[i]);
-						char* temp = names[sm_get_server_client_amount(server) - 1];
-						names[i] = temp;
-						names = realloc(names, (sm_get_server_client_amount(server) - 1) * sizeof(char*));
-						sm_client_free(server, i);
-
-						for(int i = 0; i < sm_get_server_client_amount(server); i++) {
-							send_to_client(server, i, &names, send_left_buf, bytes_send_left);
+						for(int i = 1; i < poll_amount; i++) {
+							send(s_poll[i].fd, send_buf, bytes_to_send, 0);
 						}
 					}
+
+					free(request_args);
 				}
 
 				else {
-					elapsed = time(NULL);
+
+					if(elapsed - start == 15) {
+						start = time(NULL);
+						elapsed = time(NULL);
+
+						if(send(s_poll[i].fd, ".", 1, 0) == -1) {
+							char send_left_buf[4096];
+							int bytes_send_left = sprintf(send_left_buf, "LEFT\177%s", names[i]);
+
+							char* temp = names[poll_amount - 1];
+							names[i] = temp;
+							names = realloc(names, (poll_amount - 1) * sizeof(char*));
+
+							if(names == NULL) {
+								fprintf(stderr, "Memory error");
+								return 5;
+							}
+
+							struct pollfd temp2 = s_poll[poll_amount - 1];
+							s_poll[i] = temp2;
+							s_poll = realloc(s_poll, (poll_amount - 1) * sizeof(struct pollfd));
+
+							if(s_poll == NULL) {
+								fprintf(stderr, "Memory error");
+								return 6;
+							}
+
+							for(int i = 1; i < poll_amount; i++) {
+								send(s_poll[i].fd, send_buf, bytes_to_send, 0);
+							}
+						}
+					}
+
+					else {
+						elapsed = time(NULL);
+					}
 				}
 			}
 		}
