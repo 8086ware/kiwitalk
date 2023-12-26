05e7b40c770970ee4e19bfd841eb47b270976d7a
8086ware <kirillos.khan08@outlook.com>
2023-12-26 02:25:29 -0500

    Process server_config.kt file

--- src/server/config.c
+++ src/server/config.c
@@ -1,7 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "config.h"
+#include <string.h>
 
-int open_config(struct Server_config* config) {
+struct Server_config open_config() {
+	struct Server_config sc = {0};
 
+	FILE* config_file = fopen("server_config.kt", "r");
+
+	char buffer[8192];
+
+	fread(buffer, sizeof(char), 8192, config_file);
+
+	char* title = malloc(4096);
+	
+	strcpy(title, strtok(buffer, "|||"));
+
+	sc.server_title = title;
+
+	fclose(config_file);
+	return sc;
 }
