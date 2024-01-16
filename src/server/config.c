350f8e3bd29be36d287a46f9b862add2223f3d8a
8086ware <kirillos.khan08@outlook.com>
2024-01-15 23:28:03 -0500

    Check if the file does not equal null and then add title

--- src/server/config.c
+++ src/server/config.c
@@ -8,16 +8,18 @@ struct Server_config open_config() {
 
 	FILE* config_file = fopen("server_config.kt", "r");
 
-	char buffer[8192];
+	if (config_file != NULL) {
+		char buffer[8192];
 
-	fread(buffer, sizeof(char), 8192, config_file);
+		fread(buffer, sizeof(char), 8192, config_file);
 
-	char* title = malloc(4096);
-	
-	strcpy(title, strtok(buffer, "|||"));
+		char* title = malloc(4096);
 
-	sc.server_title = title;
+		strcpy(title, strtok(buffer, "|||"));
 
-	fclose(config_file);
+		sc.server_title = title;
+
+		fclose(config_file);
+	}
 	return sc;
 }
